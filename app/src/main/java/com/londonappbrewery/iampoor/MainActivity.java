9c24da7408d3df6fa23a1e3cce771818cd7f52cf
IssiahB <jarzarr@gmail.com>
2020-11-16 20:03:53 -0700

    Task Complete

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -3,6 +3,8 @@ package com.londonappbrewery.iampoor;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changeTitle(View view) {
+        TextView title = (TextView) findViewById(R.id.title);
+        String titleValue = title.getText().toString();
+
+        if (titleValue.equalsIgnoreCase(getResources().getString(R.string.title))) {
+            title.setText("Clump Of Coal!");
+        } else {
+            title.setText(getResources().getText(R.string.title));
+        }
     }
 }
